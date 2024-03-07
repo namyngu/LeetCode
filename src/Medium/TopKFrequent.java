4969d9b232236d35bef08f15cf1d987e31bd576a
namyngu <namy.ngu@gmail.com>
2024-03-07 22:56:08 +1100

    Top K Frequent Elements - in progress

--- src/Medium/TopKFrequent.java
+++ src/Medium/TopKFrequent.java
@@ -12,8 +12,18 @@ package Medium;
 //    Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class TopKFrequent {
+
+    public static void main(String[] args) {
+        TopKFrequent start = new TopKFrequent();
+        int[] solution = start.topkFrequent(new int[]{4, 1, -1, 2, -1, 2, 3}, 2);
+
+        System.out.println(start.toString(solution));
+
+    }
 
     //Strat 1 - loop thru array and count integer occurrences - map it to a hashmap.
     // Keep track of the top k (e.g. top 5) occurrences.
@@ -22,27 +32,71 @@ public class TopKFrequent {
         // Integer, occurrences
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        // col1  = number
-        // col2 = frequency - most to least frequent
-        int[][] frequentNums = new int[k][2];
+        // HashMap of most frequent integers
+        HashMap<Integer, Integer> mostFrequent = new HashMap<>();
 
-
+        // Loop through array and count integer occurrences - map it to hashmap.
         for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
+            int currentNum = nums[i];
 
-            if (map.containsKey(num)) {
-                map.put(num, map.get(num) + 1);
+            if (map.containsKey(currentNum)) {
+                map.put(currentNum, map.get(currentNum) + 1);
 
             }
             else {
-                map.put(num, 1);
+                map.put(currentNum, 1);
             }
 
             // Check if num is frequent
-            int frequency = map.get(num);
-            if (num)
+            int frequency = map.get(currentNum);
+
+            // If number is already most frequent
+            // OR if there's not enough frequent integers
+            if (mostFrequent.containsKey(currentNum) || mostFrequent.size() < k) {
+                mostFrequent.put(currentNum, frequency);
+            }
+
+            else {
+                // If currentFreq is > than current entry, then
+                // We know that the current entry is the least frequent or equivalent to the least frequent
+                // since currentNum can only increment by one.
+                // We ignore currentNum if it's equivalent to the current entry - since mostFrequent array is full
+
+                //Create an iterator - because removing items from a HashMap while iterating through it is a problem
+                Iterator<Map.Entry<Integer, Integer>> iterator = mostFrequent.entrySet().iterator();
+                boolean isFrequent = false;
+                while (iterator.hasNext()) {
+                    Map.Entry<Integer, Integer> entry = iterator.next();
+                    if (frequency > entry.getValue()) {
+
+                        iterator.remove();
+                        break;
+                    }
+                }
+
+                if (isFrequent == true) {
+                    mostFrequent.put(currentNum, frequency);
+                }
+            }
         }
 
+
+        int[] solution = new int[k];
+        int i = 0;
+        for (int key : mostFrequent.keySet()) {
+            solution[i] = key;
+            i++;
+        }
+        return solution;
     }
 
+    public String toString(int[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i]);
+            sb.append(", ");
+        }
+
+        return sb.toString();
+    }
 }
