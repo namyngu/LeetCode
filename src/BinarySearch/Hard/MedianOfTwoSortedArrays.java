32d238f3d611f302f5e42dd8ce9fdc3b2018e0fc
Nam Nguyen <namn@radterm.local>
2025-11-24 17:28:18 +1100

    Update MedianOfTwoSortedArrays

--- src/BinarySearch/Hard/MedianOfTwoSortedArrays.java
+++ src/BinarySearch/Hard/MedianOfTwoSortedArrays.java
@@ -34,6 +34,16 @@ package BinarySearch.Hard;
 
 public class MedianOfTwoSortedArrays {
 
+    public static void main(String[] args) {
+        MedianOfTwoSortedArrays start = new MedianOfTwoSortedArrays();
+
+        int[] nums1 = {1,3};
+        int[] nums2 = {2};
+
+        double ans = start.findMedianSortedArrays(nums1, nums2);
+        System.out.println("The answer is " + ans);
+    }
+
     // Strategy: Pick the middle number in the smaller sized array.
     // Check the index it will be located in the larger array using binary search.
     // Compare this index with the index of the median if the arrays are merged.
@@ -49,11 +59,10 @@ public class MedianOfTwoSortedArrays {
 
             int medianIndex = (int) Math.floor(lenTotal / 2.0);
 
-            // Pick a number from the smaller array.
             int left = 0;
             int right = len1 - 1;
 
-            // binary search smaller array for a number
+            // binary search array 1 for a number
             while (left <= right) {
                 int middle = (int) Math.floor((right - left) / 2.0);
 
@@ -93,14 +102,86 @@ public class MedianOfTwoSortedArrays {
                 }
             }
         }
-
         else {
-            // if merged array is even
-            int medianIndex2 = lenTotal / 2;
-            int medianIndex1 = medianIndex2 - 1;
+            // the merged array is even
+            int medianIndexR = (int) Math.floor(lenTotal / 2.0);
+            int medianIndexL = medianIndexR - 1;
+            int medianL = -9999;
+            int medianR = -9999;
 
+            int left = 0;
+            int right = len1 - 1;
+
+            // Pick a number from array 1
+            while (left <= right) {
+                int middle = (int) Math.floor((right - left) / 2.0) + left;
+
+                // binary search second array for index of chosen number.
+                int foundIndex = findIndex(nums2, nums1[middle], middle);
+
+                if (foundIndex < medianIndexL) {
+                    left = middle + 1;
+                }
+                else if (foundIndex > medianIndexR) {
+                    right = middle - 1;
+                }
+                else if (foundIndex == medianIndexL) {
+                    medianL = nums1[middle];
+                    if (medianR != -9999) {
+                        return ((double) (medianL + medianR) / 2.0);
+                    }
+
+                    left = middle + 1;
+                }
+                else {
+                    medianR = nums1[middle];
+                    if (medianL != -9999) {
+                        return ((double) (medianL + medianR) / 2.0);
+                    }
+
+                    right = middle - 1;
+                }
+            }
+
+            // check second array
+            left = 0;
+            right = len2 - 1;
+
+            // binary search second array for a number
+            while (left <= right) {
+                int middle = (int) Math.floor((right - left) / 2.0);
+
+                // binary search second array for index of chosen number.
+                int foundIndex = findIndex(nums1, nums2[middle], middle);
+
+                if (foundIndex < medianIndexL) {
+                    left = middle + 1;
+                }
+                else if (foundIndex > medianIndexR) {
+                    right = middle - 1;
+                }
+                else if (foundIndex == medianIndexL) {
+                    medianL = nums2[middle];
+                    if (medianR != -9999) {
+                        return ((double) (medianL + medianR) / 2.0);
+                    }
+
+                    left = middle + 1;
+                }
+                else {
+                    medianR = nums2[middle];
+                    if (medianL != -9999) {
+                        return ((double) (medianL + medianR) / 2.0);
+                    }
+
+                    right = middle - 1;
+                }
+            }
         }
+
+        return -9999;
     }
+
 
     // For a given chosen number find its index if the two arrays are merged.
     int findIndex(int[] arr, int chosenNum, int indexOffset) {
@@ -121,12 +202,12 @@ public class MedianOfTwoSortedArrays {
             }
         }
 
-        // left should = right
+        // left and right are equal
         if (chosenNum > arr[left]) {
             return left + 1 + indexOffset;
         }
         else {
-            return left - 1 + indexOffset;
+            return left + indexOffset;
         }
     }
 }
